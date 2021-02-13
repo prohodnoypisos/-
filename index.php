7727761f51b9fa345f33ee2cda390531d1f933c9
Муравейник <79005281+prohodnoypisos@users.noreply.github.com>
2021-02-13 14:25:30 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title> Продажа пенисов - не дорого! </title>
+    <link rel="stylesheet" type="text/css" href="">
+</head>
+</head>
+<body>
+<div class="main">
+    <header>Оно работает?</header>  
+</div>
+</body>
+</html>
